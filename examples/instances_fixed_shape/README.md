de95159e7ad968b8d64ce7eb9a2ae2855a83fb74
Çetin ARDAL <6201562+kral2@users.noreply.github.com>
2021-03-02 00:57:37 +0100

    update examples for Flex Instances (#53)
    - Change documentation format for AsciiDoc
    - CONTRIBUTING.adoc updated from module vcn inputs
    - Moves inputs from README to doc/terraformoptions.adoc

---
+++ examples/instances_fixed_shape/README.md
@@ -0,0 +1,35 @@
+# Creating Compute Instances using fixed shape
+
+This example illustrates how to use this module to creates compute instances using Fixed Shape, and optionally provision and attach a block volume to the created instances.
+
+Two modules will be configured:
+
+- the first module will create 1 instance (shape VM.Standard2.1) with 1 Block Volume (50GB) attached to it
+- the second module will create 1 instances (shape VM.Standard2.1) with no additional Block Volume
+
+## Prerequisites
+
+You will need to collect the following information before you start:
+
+1. your OCI provider authentication values
+2. a compartment OCID in which the instances will be created
+3. a subnet OCID to which the instance's primary VNICs will be attached
+
+For detailed instructions, see [docs/prerequisites.adoc]
+
+## Using this example with Terraform cli
+
+Prepare one [Terraform Variable Definition file] named `terraform.tfvars` with the required authentication information.
+
+*TIP: You can rename and configure `terraform.tfvars.example` from this example's folder.*
+
+Then apply the example using the following commands:
+
+```shell
+> terraform init
+> terraform plan
+> terraform apply
+```
+
+[Terraform Variable Definition file]:https://www.terraform.io/docs/language/values/variables.html#variable-definitions-tfvars-files
+[docs/prerequisites.adoc]:https://github.com/oracle-terraform-modules/terraform-oci-compute-instance/blob/master/docs/prerequisites.adoc
--- variables.tf
+++ variables.tf
@@ -1,50 +1,54 @@
 // Copyright (c) 2018, 2021, Oracle and/or its affiliates.
 
+# general oci parameters
+
 variable "compartment_ocid" {
-  description = "(Required) (Updatable) The OCID of the compartment where to create all resources"
+  description = "(Updatable) The OCID of the compartment where to create all resources"
   type        = string
 }
 
+variable "instance_timeout" {
+  description = "Timeout setting for creating instance."
+  type        = string
+  default     = "25m"
+}
+
+# compute instance parameters
+
 variable "ad_number" {
-  description = "(Optional) The availability domain number of the instance. If none is provided, it will start with AD-1 and continue in round-robin."
+  description = "The availability domain number of the instance. If none is provided, it will start with AD-1 and continue in round-robin."
   type        = number
   default     = null
 }
 
 # variable "fd_number" {
 #   // for future use, adding fault domain support
-#   description = "(Optional) (Updatable) The fault domain of the instance."
+#   description = "(Updatable) The fault domain of the instance."
 #   type        = number
 #   default     = null
 # }
 
+variable "instance_count" {
+  description = "Number of identical instances to launch from a single module."
+  type        = number
+  default     = 1
+}
+
 variable "instance_display_name" {
-  description = "(Optional) (Updatable) A user-friendly name for the instance. Does not have to be unique, and it's changeable."
+  description = "(Updatable) A user-friendly name for the instance. Does not have to be unique, and it's changeable."
   type        = string
   default     = ""
 }
 
-variable "extended_metadata" {
-  description = "(Optional) (Updatable) Additional metadata key/value pairs that you provide."
-  type        = map(any)
-  default     = {}
-}
-
-variable "ipxe_script" {
-  description = "(Optional) The iPXE script which to continue the boot process on the instance."
-  type        = string
+variable "instance_flex_memory_in_gbs" {
+  type        = number
+  description = "(Updatable) The total amount of memory available to the instance, in gigabytes."
   default     = null
 }
 
-variable "preserve_boot_volume" {
-  description = "Specifies whether to delete or preserve the boot volume when terminating an instance."
-  type        = bool
-  default     = false
-}
-
-variable "boot_volume_size_in_gbs" {
-  description = "The size of the boot volume in GBs."
+variable "instance_flex_ocpus" {
   type        = number
+  description = "(Updatable) The total number of OCPUs available to the instance."
   default     = null
 }
 
@@ -54,17 +58,43 @@ variable "shape" {
   default     = "VM.Standard2.1"
 }
 
-variable "instance_flex_memory_in_gbs" {
-  type        = number
-  description = "(Optional) (Updatable) The total amount of memory available to the instance, in gigabytes."
+variable "source_ocid" {
+  description = "The OCID of an image or a boot volume to use, depending on the value of source_type."
+  type        = string
+}
+
+variable "source_type" {
+  description = "The source type for the instance."
+  type        = string
+  default     = "image"
+}
+
+# operating system parameters
+
+variable "extended_metadata" {
+  description = "(Updatable) Additional metadata key/value pairs that you provide."
+  type        = map(any)
+  default     = {}
+}
+
+variable "resource_platform" {
+  description = "Platform to create resources in."
+  type        = string
+  default     = "linux"
+}
+
+variable "ssh_authorized_keys" {
+  description = "Public SSH keys path to be included in the ~/.ssh/authorized_keys file for the default user on the instance."
+  type        = string
+}
+
+variable "user_data" {
+  description = "Provide your own base64-encoded data to be used by Cloud-Init to run custom scripts or provide custom Cloud-Init configuration."
+  type        = string
   default     = null
 }
 
-variable "instance_flex_ocpus" {
-  type        = number
-  description = "(Optional) (Updatable) The total number of OCPUs available to the instance."
-  default     = null
-}
+# networking parameters
 
 variable "assign_public_ip" {
   description = "Whether the VNIC should be assigned a public IP address."
@@ -72,16 +102,16 @@ variable "assign_public_ip" {
   default     = false
 }
 
-variable "vnic_name" {
-  description = "A user-friendly name for the VNIC."
-  type        = string
-  default     = ""
-}
-
 variable "hostname_label" {
   description = "The hostname for the VNIC's primary private IP."
   type        = string
   default     = ""
+}
+
+variable "ipxe_script" {
+  description = "(Optional) The iPXE script which to continue the boot process on the instance."
+  type        = string
+  default     = null
 }
 
 variable "private_ips" {
@@ -101,38 +131,18 @@ variable "subnet_ocids" {
   type        = list(string)
 }
 
-variable "ssh_authorized_keys" {
-  description = "Public SSH keys path to be included in the ~/.ssh/authorized_keys file for the default user on the instance."
+variable "vnic_name" {
+  description = "A user-friendly name for the VNIC."
   type        = string
+  default     = ""
 }
 
-variable "user_data" {
-  description = "Provide your own base64-encoded data to be used by Cloud-Init to run custom scripts or provide custom Cloud-Init configuration."
-  type        = string
-  default     = null
-}
+# storage parameters
 
-variable "source_ocid" {
-  description = "The OCID of an image or a boot volume to use, depending on the value of source_type."
+variable "attachment_type" {
+  description = "(Optional) The type of volume. The only supported values are iscsi and paravirtualized."
   type        = string
-}
-
-variable "source_type" {
-  description = "The source type for the instance."
-  type        = string
-  default     = "image"
-}
-
-variable "instance_timeout" {
-  description = "Timeout setting for creating instance."
-  type        = string
-  default     = "25m"
-}
-
-variable "instance_count" {
-  description = "Number of instances to launch."
-  type        = number
-  default     = 1
+  default     = "paravirtualized"
 }
 
 variable "block_storage_sizes_in_gbs" {
@@ -148,20 +158,20 @@ variable "block_storage_sizes_in_gbs" {
 #   default     = true
 # }
 
-variable "attachment_type" {
-  description = "(Optional) The type of volume. The only supported values are iscsi and paravirtualized."
-  type        = string
-  default     = "paravirtualized"
+variable "boot_volume_size_in_gbs" {
+  description = "The size of the boot volume in GBs."
+  type        = number
+  default     = null
+}
+
+variable "preserve_boot_volume" {
+  description = "Specifies whether to delete or preserve the boot volume when terminating an instance."
+  type        = bool
+  default     = false
 }
 
 variable "use_chap" {
   description = "(Applicable when attachment_type=iscsi) Whether to use CHAP authentication for the volume attachment."
   type        = bool
   default     = false
-}
-
-variable "resource_platform" {
-  description = "Platform to create resources in."
-  type        = string
-  default     = "linux"
 }
